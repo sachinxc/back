798fb1276d634694e55ebb0111cf7cbf035e0343
sachinxc <160459008+sachinxc@users.noreply.github.com>
2024-09-25 13:40:57 +0530

    Update post.js

--- models/post.js
+++ models/post.js
@@ -1,3 +1,39 @@
+/*const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Post = sequelize.define(
+  "Post",
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.ENUM(
+        "Social Welfare",
+        "Animal Welfare",
+        "Environmental",
+        "Innovation",
+        "Other"
+      ),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = Post;*/
+
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
@@ -24,6 +60,10 @@ const Post = sequelize.define(
     },
     location: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    activityLog: {
+      type: DataTypes.TEXT,
       allowNull: false,
     },
   },
